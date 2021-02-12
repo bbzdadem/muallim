2e623efa1792f0a4725952b7d76ca0d601a80c55
Adam Baba <bbzdadem@gmail.com>
2021-02-12 19:58:08 +0400

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,13 @@ window.addEventListener("scroll", function() {
 })
 
 var modal = document.getElementById("modal-wrapper");
+var modalsecond = document.getElementById("modal-wrapper-sec");
+var modalthird = document.getElementById("modal-wrapper-thr");
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || event.target == modalsecond || event.target == modalthird) {
         modal.style.display = "none";
+        modalsecond.style.display = "none";
+        modalthird.style.display = "none";
     }
 }
 
@@ -23,8 +27,43 @@ function show() {
     document.getElementById('modal-wrapper').style.display = 'block'
 }
 
+function showsign() {
+    document.getElementById('modal-wrapper-sec').style.display = 'block';
+}
+
+
+function showfrgt() {
+    document.getElementById('modal-wrapper-sec').style.display = 'none';
+    document.getElementById('modal-wrapper-thr').style.display = 'block';
+}
+
+
+function hidefrgt() {
+    document.getElementById('modal-wrapper-sec').style.display = 'block';
+    document.getElementById('modal-wrapper-thr').style.display = 'none';
+}
 
 function shownd() {
-    document.getElementById('join_first').style.display = 'none';
-    document.getElementById('join_second').style.display = 'block'
+    var x = document.getElementById('modal-wrapper-sec');
+    var y = document.getElementById('modal-wrapper');
+    if (y.style.display == 'block' && x.style.display == 'none') {
+        y.style.display = 'none'
+        x.style.display = 'block'
+    } else {
+        x.style.display = 'none'
+        y.style.display = 'block'
+    }
+}
+
+
+function showrd() {
+    var x = document.getElementById('modal-wrapper-sec');
+    var y = document.getElementById('modal-wrapper');
+    if (x.style.display == 'block' && y.style.display == 'none') {
+        x.style.display = 'none'
+        y.style.display = 'block'
+    } else {
+        y.style.display = 'none'
+        x.style.display = 'block'
+    }
 }
